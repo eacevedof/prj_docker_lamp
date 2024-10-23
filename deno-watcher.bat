4797662c8147f49e3b818dc9725450d0dc6d6efe
Eduardo A.F. <eacevedof@yahoo.es>
2024-10-23 20:34:33 +0200

    deno-translator watcher

--- deno-watcher.bat
+++ deno-watcher.bat
@@ -1,26 +1,37 @@
+REM must be run as administrator
 @echo off
 setlocal
 
-REM Get the directory of the current script
-set SCRIPT_DIR=%~dp0
+REM Define the directories to watch and their corresponding commands
+set WATCH_DIR1=%~dp0deno\www\app-dotlake\Modules
+set COMMAND1=docker restart cont-deno-dotlake
 
-REM Set the watch directory relative to the script directory
-set WATCH_DIR=%SCRIPT_DIR%deno\www\app-dotlake\Modules
-set COMMAND=docker restart cont-deno-dotlake
+set WATCH_DIR2=%~dp0deno\www\app-translator\Modules
+set COMMAND2=docker restart cont-deno-translator
 
-echo Watching directory: %WATCH_DIR%
-echo Command to execute: %COMMAND%
+echo Watching directories:
+echo %WATCH_DIR1%
+echo %WATCH_DIR2%
 echo.
 
 REM Use PowerShell to watch for changes
 powershell -Command ^
-    "$watcher = New-Object System.IO.FileSystemWatcher '%WATCH_DIR%';" ^
-    "$watcher.IncludeSubdirectories = $true;" ^
-    "$watcher.NotifyFilter = [System.IO.NotifyFilters]'FileName, LastWrite';" ^
-    "$action = { Write-Output 'Change detected: ' + $_.FullPath; Start-Process cmd -ArgumentList '/c %COMMAND%' -WindowStyle Hidden };" ^
-    "Register-ObjectEvent $watcher 'Changed' -Action $action;" ^
-    "Register-ObjectEvent $watcher 'Created' -Action $action;" ^
-    "Register-ObjectEvent $watcher 'Deleted' -Action $action;" ^
-    "Register-ObjectEvent $watcher 'Renamed' -Action $action;" ^
-    "$watcher.EnableRaisingEvents = $true;" ^
+    "$watcher1 = New-Object System.IO.FileSystemWatcher '%WATCH_DIR1%';" ^
+    "$watcher1.IncludeSubdirectories = $true;" ^
+    "$watcher1.NotifyFilter = [System.IO.NotifyFilters]'FileName, LastWrite';" ^
+    "$action1 = { Write-Output 'Change detected in %WATCH_DIR1%: ' + $_.FullPath; Start-Process cmd -ArgumentList '/c %COMMAND1%' -WindowStyle Hidden };" ^
+    "Register-ObjectEvent $watcher1 'Changed' -Action $action1;" ^
+    "Register-ObjectEvent $watcher1 'Created' -Action $action1;" ^
+    "Register-ObjectEvent $watcher1 'Deleted' -Action $action1;" ^
+    "Register-ObjectEvent $watcher1 'Renamed' -Action $action1;" ^
+    "$watcher1.EnableRaisingEvents = $true;" ^
+    "$watcher2 = New-Object System.IO.FileSystemWatcher '%WATCH_DIR2%';" ^
+    "$watcher2.IncludeSubdirectories = $true;" ^
+    "$watcher2.NotifyFilter = [System.IO.NotifyFilters]'FileName, LastWrite';" ^
+    "$action2 = { Write-Output 'Change detected in %WATCH_DIR2%: ' + $_.FullPath; Start-Process cmd -ArgumentList '/c %COMMAND2%' -WindowStyle Hidden };" ^
+    "Register-ObjectEvent $watcher2 'Changed' -Action $action2;" ^
+    "Register-ObjectEvent $watcher2 'Created' -Action $action2;" ^
+    "Register-ObjectEvent $watcher2 'Deleted' -Action $action2;" ^
+    "Register-ObjectEvent $watcher2 'Renamed' -Action $action2;" ^
+    "$watcher2.EnableRaisingEvents = $true;" ^
     "while ($true) { Start-Sleep -Seconds 1; }"
